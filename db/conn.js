04fa184557259a9c2ca4200cf7809f81fb8306e4
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-21 11:44:02 -0500

    dotenv added

--- db/conn.js
+++ db/conn.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
     user: process.env.DB_USER,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PASSWORD
+    port: process.env.DB_PORT
 });
 
 exports.pool = pool;
