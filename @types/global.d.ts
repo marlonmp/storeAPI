a51e3d65c9929b71ded81a8f846c45a6eb6b284b
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-21 15:49:56 -0500

    product model types

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -5,10 +5,26 @@ declare global {
     // Globals
 
     type error = Error | null;
+
+    interface JSONObject {
+        [key: string]: any
+    }
     
     // db/*.js
 
     type QueryRes = QueryResult<any> | null;
 
     type Exec = (query: string, args: any[]) => { err: error, res: QueryRes };
+
+    // model/*.js
+
+    type Product = {
+        id: number,
+    
+        name: string,
+        description: string,
+    
+        price: number,
+        sales: number,
+    };
 }
