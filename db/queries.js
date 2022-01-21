bbf975bb1efd9bc2c1a89f2d6785d67472ca1edb
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-21 15:48:53 -0500

    formatSets added

--- db/queries.js
+++ db/queries.js
@@ -20,6 +20,31 @@ async function exec(query, args) {
     }
 }
 
+function formatSets(obj = {}) {
+
+    let query = '';
+
+    const args = [];
+
+    let i = 1;
+
+    Object.entries(obj).forEach(([key, value]) => {
+
+        if (value) {
+            query += ` "${key}" = $${i},`;
+            
+            args.push(value);
+
+            i++;
+        }
+    });
+
+    query = query.slice(0, -1);
+
+    return { query, args };
+}
+
 module.exports = {
-    exec
+    exec,
+    formatSets
 }
