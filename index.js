708bb4d6be66ab664415c197ddf6cc0eea47166f
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-21 17:09:15 -0500

    product routes added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.use(require('./routes'));
+
 const listener = app.listen(
     process.env.SERVER_PORT,
     process.env.SERVER_HOST,
