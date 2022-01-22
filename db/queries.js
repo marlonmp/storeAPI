2ebf26494cdfbc1ce09c5645a6555120e76053b4
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-22 18:13:45 -0500

    updateQueryFormatter function updated

--- db/queries.js
+++ db/queries.js
@@ -16,31 +16,31 @@ async function exec(query, args) {
     }
 }
 
-function formatSets(obj = {}) {
-
-    let query = '';
-
-    const args = [];
-
-    let i = 1;
-
+function updateQueryFormatter(table = '', id = 0, obj = {}) {
+    
+    let query = `UPDATE "${table}" SET`;
+    
+    const args = [id];
+    
+    let i = 2;
+    
     Object.entries(obj).forEach(([key, value]) => {
-
+        
         if (value) {
             query += ` "${key}" = $${i},`;
             
             args.push(value);
-
+            
             i++;
         }
     });
-
-    query = query.slice(0, -1);
+    
+    query = query.slice(0, -1) + ` WHERE "id" = $1;`;
 
     return { query, args };
 }
 
 module.exports = {
     exec,
-    formatSets
+    updateQueryFormatter
 }
