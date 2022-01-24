c5a684241028255572d48d93a3207deb1e3fb744
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-24 15:17:17 -0500

    cookie-parser implemented

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(require('./routes'));
 
