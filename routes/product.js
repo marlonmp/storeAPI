0d9dffa2208b85426251cf0d7f3d735406fbe998
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-01-31 11:33:35 -0500

    middlewares added

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const productCtl = require('../controller/prouct');
+const productCtl = require('../controller/product');
 
-router.post('/', productCtl.create);
+const auth = require('../middleware/auth');
+const validate = require('../middleware/validations');
 
-router.get('/', productCtl.search);
+router.get('/', auth.verifyToken, validate.createProduct, productCtl.searchProduct);
 
-router.get('/:id', productCtl.getById);
+router.get('/:id', auth.verifyToken, validate.paramId, productCtl.getById);
 
-router.put('/:id', productCtl.update);
+router.post('/', auth.verifyToken, auth.verifyIfIsOwner, validate.createProduct, productCtl.create);
 
-router.delete('/:id', productCtl.remove);
+router.put('/:id', auth.verifyToken, auth.verifyIfIsOwner, validate.updateProduct, productCtl.update);
+
+router.delete('/:id', auth.verifyToken, auth.verifyIfIsOwner, validate.paramId, productCtl.remove);
 
 module.exports = router;
