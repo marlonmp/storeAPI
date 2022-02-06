0405586d0eac0d90654505157ac71dff7459bd5f
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-02-05 20:09:41 -0500

    account roures added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/account', require('./account'));
+
 router.use('/product', require('./product'));
 
 module.exports = router;
