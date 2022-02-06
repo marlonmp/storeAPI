44c52a9e56a757f625fed6305f2cecb19fb5959e
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-02-05 19:48:56 -0500

    hash middleware added

---
+++ middleware/hashPassword.js
@@ -0,0 +1,18 @@
+const argon2 = require('../lib/argon2');
+
+function hash(req, res, next) {
+
+    const { password } = req.checkedBody.user;
+
+    const { err, hash } = argon2.hash(password);
+
+    if (!err) {
+        console.log('[ERROR] err:', err);
+
+        res.sendStatus(500); // Internal server error
+    }
+
+    req.checkedBody.user.password = hash;
+
+    next();
+}
