be35db8a812a9ab7e1827cb272794f3b3719e100
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-02-05 19:28:35 -0500

    user model updated

--- model/user.js
+++ model/user.js
@@ -3,21 +3,21 @@ const { exec, updateQueryFormatter } = require('../db/queries');
 /** @param { User } user */
 async function insert(user = {}) {
 
-    const query = `INSERT INTO "user" ("role_id","first_name","last_name","email","password") VALUES ($1,$2,$3,$4,$5);`;
+    const query = `CALL new_user($1,$2,$3,$4,$5);`;
 
     return { err, rowCount } = await exec(query, [user.role, user.firstName, user.lastName, user.email, user.password]);
 }
 
 async function signIn(email = '') {
 
-    const query = `SELECT "id", "role_id", "password" FROM "user" WHERE "email" = $1`;
+    const query = `SELECT sign_in($1)`;
 
     return { err, rows } = await exec(query, [email]);
 }
 
 async function getById(id = 0) {
 
-    const query = `SELECT "role_id","first_name","last_name","email" FROM "user" WHERE "id" = $1;`;
+    const query = `SELECT get_user($1);`;
 
     return { err, rows } = await exec(query, [id]);
 }
@@ -25,14 +25,14 @@ async function getById(id = 0) {
 /** @param { User } newUser */
 async function update(id = 0, newUser = {}) {
 
-    const { query, args } = updateQueryFormatter("user", id, newUser);
+    const query = `CALL update_user($1,$2,$3,$4,$5);`;
 
-    return { err, rowCount } = await exec(query, args);
+    return { err, rowCount } = await exec(query, [id, newUser.firstName, newUser.lastName, newUser.email, newUser.password]);
 }
 
 async function remove(id = 0) {
 
-    const query = `DELETE FROM "user" WHERE "id" = $1;`;
+    const query = `CALL delete_user($1);`;
 
     return { err, rowCount } = await exec(query, [id]);
 }
