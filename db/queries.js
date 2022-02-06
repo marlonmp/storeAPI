b10493e23a18db23227ac94e7d08ed4ca0bc84f1
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2022-02-05 19:27:12 -0500

    updateQueryFormatter function deprecated

--- db/queries.js
+++ db/queries.js
@@ -16,31 +16,6 @@ async function exec(query, args) {
     }
 }
 
-function updateQueryFormatter(table = '', id = 0, obj = {}) {
-    
-    let query = `UPDATE "${table}" SET`;
-    
-    const args = [id];
-    
-    let i = 2;
-    
-    Object.entries(obj).forEach(([key, value]) => {
-        
-        if (value) {
-            query += ` "${key}" = $${i},`;
-            
-            args.push(value);
-            
-            i++;
-        }
-    });
-    
-    query = query.slice(0, -1) + ` WHERE "id" = $1;`;
-
-    return { query, args };
-}
-
 module.exports = {
-    exec,
-    updateQueryFormatter
+    exec
 }
